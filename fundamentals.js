a6cac3ad6ec0bda560d0defc033f1b5e6005e980
ym y <ymy@ymdeMacBook-Air.wit.ie>
2023-09-21 13:19:46 +0100

    Dynamic properties.

--- fundamentals.js
+++ fundamentals.js
@@ -1,10 +1,19 @@
 const aCar = {
-    owner : 'Joe Bloggs',
-    type : {
-        make : 'Toyota',
-        model : 'Corolla',
-        cc : 1.8
+    owner: "Joe Bloggs",
+    type: {
+      make: "Toyota",
+      model: "Corolla",
+      cc: 1.8
     },
-    registration : '201WD1058'
-};
-console.log(aCar.owner + ' drives a ' + aCar.type.make);
+    registration: { year: 201, county: "WD", number: 1058 }
+  };
+  
+  console.log(aCar.owner + " drives a " + aCar.type.make);
+  console.log(
+    "Reg. = " +
+      aCar.registration.year +
+      "-" +
+      aCar.registration.county +
+      "-" +
+      aCar.registration.number
+  );

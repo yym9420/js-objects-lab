b2211cd2771d01f25e7f781c1dd34558bc635fc4
ym y <ymy@ymdeMacBook-Air.wit.ie>
2023-09-21 13:18:09 +0100

    Nested objects.

--- fundamentals.js
+++ fundamentals.js
@@ -1,7 +1,10 @@
 const aCar = {
-    owner : "Joe Bloggs",
-    type : 'Toyota Corolla 1.8',
+    owner : 'Joe Bloggs',
+    type : {
+        make : 'Toyota',
+        model : 'Corolla',
+        cc : 1.8
+    },
     registration : '201WD1058'
-  };
-  
-  console.log(aCar.owner);
+};
+console.log(aCar.owner + ' drives a ' + aCar.type.make);

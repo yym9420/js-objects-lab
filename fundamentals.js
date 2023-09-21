73b01f1444d9beb79f3e4b57f4d194cf94fc5d4f
ym y <ymy@ymdeMacBook-Air.wit.ie>
2023-09-21 13:21:07 +0100

    Arrays.

--- fundamentals.js
+++ fundamentals.js
@@ -1,19 +1,29 @@
 const aCar = {
-    owner: "Joe Bloggs",
-    type: {
-      make: "Toyota",
-      model: "Corolla",
-      cc: 1.8
-    },
-    registration: { year: 201, county: "WD", number: 1058 }
-  };
-  
-  console.log(aCar.owner + " drives a " + aCar.type.make);
-  console.log(
-    "Reg. = " +
-      aCar.registration.year +
-      "-" +
-      aCar.registration.county +
-      "-" +
-      aCar.registration.number
-  );
+    
+ };
+ 
+ console.log(aCar.owner + " drives a " + aCar.type.make);
+ console.log(
+   "Reg. = " +
+     aCar.registration.year +
+     "-" +
+     aCar.registration.county +
+     "-" +
+     aCar.registration.number
+ );
+ 
+ aCar.milage = 10000
+ aCar.color = {
+   exterior: "red",
+   interior: { texture: "leather", shade: "cream" }
+ }
+ 
+ console.log(
+   "It is a " +
+     aCar.color.exterior +
+     " car, " +
+     aCar.milage +
+     " milage, with " +
+     aCar.color.interior.texture +
+     " interior."
+ );

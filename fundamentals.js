9fcac6702c191ff1453e08c30935ba34cf2404e6
ym y <ymy@ymdeMacBook-Air.wit.ie>
2023-09-21 13:22:11 +0100

    Loop constructs.

--- fundamentals.js
+++ fundamentals.js
@@ -1,29 +1,15 @@
 const aCar = {
+    owner: "Joe Bloggs",
+    previous_owners: [
+      { name: "Pat Smith", address: "1 Main Street" },
+      { name: "Sheila Dwyer", address: "2 High Street" }
+    ],
     
- };
- 
- console.log(aCar.owner + " drives a " + aCar.type.make);
- console.log(
-   "Reg. = " +
-     aCar.registration.year +
-     "-" +
-     aCar.registration.county +
-     "-" +
-     aCar.registration.number
- );
- 
- aCar.milage = 10000
- aCar.color = {
-   exterior: "red",
-   interior: { texture: "leather", shade: "cream" }
- }
- 
- console.log(
-   "It is a " +
-     aCar.color.exterior +
-     " car, " +
-     aCar.milage +
-     " milage, with " +
-     aCar.color.interior.texture +
-     " interior."
- );
+  }
+  
+  console.log(
+    "First owner: " +
+      aCar.previous_owners[0].name +
+      ' - ' +
+      aCar.previous_owners[0].address
+  )
